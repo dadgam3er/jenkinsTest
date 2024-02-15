f075b1c176b0677490b1a5263b377a2d27c37a4c
dadgam3er <i.l.y.e.s@live.fr>
2024-02-15 14:47:16 -0500

    InterviewProject

--- src/main/java/PrepSession/commonAPI.java
+++ src/main/java/PrepSession/commonAPI.java
@@ -1,28 +1,57 @@
 package PrepSession;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 /**
- * Hello world!
+ * Hello
  *
  */
-public class commonAPI
-{
-    WebDriver driver;
-    @BeforeTest
-    public void buildUP(){
-        System.setProperty("web-driver.gecko.driver",System.getProperty("user.dir")+"/Drivers/geckodriver");
-        driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-        driver.get("https://google.com");
-    }
+public class commonAPI {
+  WebDriver driver;
 
-    @AfterTest
-    public void tearDown(){
-        driver.close();
+  @BeforeTest
+  @Parameters("browser")
+  public void buildUP(String browser) {
+    if (browser.equalsIgnoreCase("firefox")) {
+      System.setProperty("web-driver.gecko.driver", System.getProperty("user.dir") + "/Drivers/geckodriver");
+      // driver = new FirefoxDriver();
+      DesiredCapabilities cap = new DesiredCapabilities();
+      cap.setCapability("browserName", "firefox");
+      try {
+        driver = new RemoteWebDriver(new URL("http://192.168.1.235:4444/wd/hub"), cap);
+      } catch (MalformedURLException e) {
+        // TODO: handle exception
+        e.printStackTrace();
+      }
+
+    } else if (browser.equalsIgnoreCase("chrome")) {
+      System.setProperty("web-driver.gecko.driver", System.getProperty("user.dir") + "/Drivers/chromedriver");
+      // driver = new ChromeDriver();
+      DesiredCapabilities cap = new DesiredCapabilities();
+      cap.setCapability("browserName", "chrome");
+      try {
+        driver = new RemoteWebDriver(new URL("http://192.168.1.235:4444/wd/hub"), cap);
+      } catch (MalformedURLException e) {
+        // TODO: handle exception
+        e.printStackTrace();
+      }
     }
+    driver.manage().window().maximize();
+    driver.get("https://google.com");
+  }
+
+  @AfterTest
+  public void tearDown() {
+    driver.close();
+  }
 }
